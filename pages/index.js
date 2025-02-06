b2abcb9f0ae2734260ff6795d8c69d2704bd4731
mattheusdev08 <mattheus.dev08@gmail.com>
2025-02-06 02:43:31 +0000

    install `jest`and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 function Home() {
-  return <h1>Oi, isso é apenas um teste. Pode ignorar...</h1>;
-}
-
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
+  return <h1>Oi, isso é apenas um teste. Pode ignorar...;</h1>;
 }
 
 export default Home;
